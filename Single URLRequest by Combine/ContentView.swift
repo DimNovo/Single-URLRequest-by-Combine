517762623c8ec79e76f96214994584d6f14b5b1e
Dmitry Novosyolov <i1710@icloud.com>
2020-01-12 13:15:47 +0200

    Initial Commit

---
+++ Single URLRequest by Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Single URLRequest by Combine
+//
+//  Created by Dmitry Novosyolov on 12/01/2020.
+//  Copyright © 2020 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
